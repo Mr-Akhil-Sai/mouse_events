593f1b8f7a048f13b2cd44c587f2498402036640
Mr.Ak <akhilsaivaraganti@gmail.com>
2021-02-19 13:42:26 +0530

    length and breadth needs to be resolved

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ function creatingDiv(x, y, startX, startY){
 	let e = document.createElement("div");
 	let body = document.querySelector("body");
 	body.appendChild(e);
-	let width = e.style.width = startX - x;
+	e.style.width = startX - x;
 	e.style.height = startY - y;
-	e.style.border = "1px solid red";
-	console.log(width);
 }

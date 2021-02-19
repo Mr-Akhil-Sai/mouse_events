ce2fb57670567c8ed8732e1d8c5d9daed85db8c9
Mr.Ak <akhilsaivaraganti@gmail.com>
2021-02-19 13:33:10 +0530

    need to work on width and heigth

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-window.addEventListener("click", (event)=>{
-	let x = event.clientX;
-	let y = event.clientY;
-	console.log(x, y);
+window.addEventListener("mousemove", (event)=>{
+	let x = event.offsetX;
+	let y = event.offsetY;
 });
-
+window.addEventListener("click", (event) => {
+	let startX = event.clientX;
+	let startY = event.clientY;
+	creatingDiv(startX, startY);
+});
+function creatingDiv(x, y, startX, startY){
+	let e = document.createElement("div");
+	let body = document.querySelector("body");
+	body.appendChild(e);
+	let width = e.style.width = startX - x;
+	e.style.height = startY - y;
+	e.style.border = "1px solid red";
+	console.log(width);
+}
